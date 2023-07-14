cdc36b5baed6a3734f5174a79d89dde4c69f0a31
Evi Sakkou <64957367+evisakkou@users.noreply.github.com>
2023-07-14 16:27:28 +0300

    Update rearange.py

--- Volume_Tank/rearange.py
+++ Volume_Tank/rearange.py
@@ -4,10 +4,8 @@ import pandas as pd
 
 df = pd.read_csv('/home/evi/Desktop/ANNIE-THESIS-2/VolumeTank/ANNIE_VertexReco/Volume_Tank/VolumeTank_Texpected1.csv')
 
-# Get the number of sets (e.g., 20)
-num_sets = 20
 
-# Create a list to store the column order
+num_sets = 20
 column_order = []
 
 # Rearrange columns by grouping X, Y, Z, and T values together
@@ -26,21 +24,16 @@ for i in range(1, num_sets + 1):
 # Select the columns in the desired order
 new_df = df[column_order]
 
-
-# Save the rearranged columns to a new CSV file
 new_df.to_csv('/home/evi/Desktop/ANNIE-THESIS-2/VolumeTank/ANNIE_VertexReco/Volume_Tank/VolumeTank_Texpected1.csv', index=False)
 
 
 #ADD COLUMNS FOR NHITS, GRIDPOINT, AND TRUE GRID COORDS
 
-# Read the existing CSV file into a DataFrame
 existing_df = pd.read_csv('/home/evi/Desktop/ANNIE-THESIS-2/VolumeTank/ANNIE_VertexReco/Volume_Tank/VolumeTank_Texpected1.csv')
-
-# Read the additional CSV file into a DataFrame
 extra_df = pd.read_csv('tankPMT_withonlyMRDcut_insidevolume2.csv')
 
 # Extract the desired columns from the extra DataFrame
-add_columns = ['totalPMTs', 'truevtxX', 'truevtxY', 'truevtxZ', 'Gridpoint', 'xc', 'yc', 'zc']  # Replace with the actual column names you want to extract
+add_columns = ['totalPMTs', 'truevtxX', 'truevtxY', 'truevtxZ', 'Gridpoint', 'xc', 'yc', 'zc']  
 extra_columns = extra_df[add_columns]
 
 # Concatenate the existing DataFrame with the extra columns
